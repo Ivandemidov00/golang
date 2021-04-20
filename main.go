70f8e906ecd90e7532d4b7fb085e572b73ea27de
ivan.demidov00@mail.ru <ivan.demidov00@mail.ru>
2021-04-20 09:31:53 +0500

    second golang

--- main.go
+++ main.go
@@ -6,10 +6,15 @@ func pl(a int16, b int16) int16 {
 	var c int16 = a + b
 	return c
 }
+func diff(a int16, b int16) int16 {
+	var c int16 = a - b
+	return c
+}
 func main() {
-	var x int16
-	x = pl(23, 34)
+	var x int16 = pl(23, 34)
 	fmt.Println(x)
 	x = 777
 	fmt.Println(x)
+	fmt.Println(diff(7, 9))
+
 }
